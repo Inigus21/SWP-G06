769e68f95207565fee156f381b37991029b5fee1
Tuann6688 <tuanlqhe181759@fpt.edu.vn>
2025-02-28 23:03:47 +0700

    create tourlistcontroller

--- src/java/utils/DBContext.java
+++ src/java/utils/DBContext.java
@@ -9,9 +9,9 @@ public class DBContext {
     private static final String dbName = "SWPver4";
     private static final String portNumber = "1433";
     private static final String instance = "";
-    private static final String userID = "sa";
+    private static final String userID = "123456";
 
-    private static final String password = "123456";
+    private static final String password = "sa";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         String url = "jdbc:sqlserver://" + serverName + ":" + portNumber
